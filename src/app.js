6ca674e9312f41a9d6fbc7a3419662a49e63bfe4
Anubhav Rana <anubhav.r@idsil.com>
2024-02-18 23:48:09 +0530

    initial-commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const app = express();
+
+//FOR JSON 
+app.use(express.json());
+
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import app from "./app.js";
+import cors from 'cors';
+
+//DOT ENV CONFIG 
+dotenv.config();
+
+//CORS CONFIG
+app.use(cors());
+
+//PORT CONNECTION
+const PORT = process.env.PORT || 7000;
+
+//DB CONFIG
+import dbConfig from './db/dbConfig.js';
+
+
+//SERVER CONNECTIONS
+const startServer = async () => {
+    try {
+        await dbConfig();
+        app.on("error", (err) => {
+            console.log("Error: --> ", err);
+        });
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.log("Mongo DB Connection Error-->", err);
+    }
+};
+
+startServer();
